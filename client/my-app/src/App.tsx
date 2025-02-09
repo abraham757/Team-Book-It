5aab1ea26d5ccc1b21f563da3f2fa8018ffa05a7
Daniel Gonzalez <dannygonzr20@gmail.com>
2025-02-09 11:56:22 -0500

    Pushing react styling to login/register forms

--- client/my-app/src/App.tsx
+++ client/my-app/src/App.tsx
@@ -1,9 +1,20 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Hero from './components/hero';
 import Features from './components/features';
 import Login from './pages/Login';
 import Register from './pages/Register';
+
+function Home() {
+  const location = useLocation();
+
+  return (
+    <>
+      <Hero />
+      {location.pathname === '/' && <Features />} {/* Only show on homepage */}
+    </>
+  );
+}
 
 function App() {
   return (
@@ -11,8 +22,7 @@ function App() {
       <Navbar />
       <div className="mx-auto pt-20 px-6 w-full">
         <Routes>
-          <Route path="/" element={<Hero />} />
-          <Route path="/features" element={<Features />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
